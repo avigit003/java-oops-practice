fc917835308d39743a98bf7c8e97c608dc89a957
Avi <gwsajan042@gmail.com>
2026-08-31 11:58:21 +0530

    Learn Java inheritance

---
+++ src/inheritance/Car.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class Car extends Vehicle {
+    void drive(){
+        System.out.println("Car drive");
+    }
+}
---
+++ src/inheritance/Vehicle.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Vehicle {
+    void start(){
+        System.out.println("Vehicle starting");
+    }
+    void stop(){
+        System.out.println("Vehicle stopping");
+    }
+}
