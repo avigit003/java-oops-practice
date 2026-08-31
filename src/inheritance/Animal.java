5e2c7983d673c8553007ea0736401ce9fa4784b0
Avi <gwsajan042@gmail.com>
2026-08-31 07:21:00 +0530

    Learn Java inheritance

---
+++ src/inheritance/Animal.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Animal {
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+
+    void sleep(){
+        System.out.println("Animal is sleeping");
+    }
+}
